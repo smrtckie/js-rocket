743469464b0a601a9d37d60ed20ec8c944c06559
smartcookie05 <sarahgschallon@icloud.com>
2023-04-16 17:15:20 -0400

    tested some js console

---
+++ examples.js
@@ -0,0 +1,38 @@
+
+//Explaining Variables//
+
+	var myName = "Sarah";
+
+	var grassIsBlue = false;
+
+	var sarahObject = {
+		firstname: 'Sarah',
+		lastname: 'Schallon',
+		favColors: ['yellow', 'orange', 'gold'],
+		yearsAlive: 15,
+		isFemale: true
+	};
+
+//Explaining Functions//
+
+	var whatIsMyFavoriteAnimal = function () {
+
+		return 'none';
+		//anything after the 1st return is ignored!!//
+
+		return 'monkey';
+	}
+
+	var scareMe = function () {
+		console.log("BOO!!!");
+	}
+
+	var area = function(width, height) {
+		return width * height;
+		//type in console "area(#,#)" to get answer//
+	}
+
+	var fullname = function(firstname, secondname, lastname){
+		return firstname + " " + secondname + " " + lastname;
+		//type in colsole with strings for each name ('Sarah', ...)//
+	}
